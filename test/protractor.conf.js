0999b8d827d1179f602af2953586c55e45a8109c
Florent Dubost <fdubost.externe@m6.fr>
2014-11-03 15:08:02 +0100

    Test with firefox

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -8,7 +8,7 @@ exports.config = {
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'firefox'
   },
 
   // Spec patterns are relative to the current working directly when
