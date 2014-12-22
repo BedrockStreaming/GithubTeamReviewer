e69a16d9a6ab1ef004c54c246e05b0a2d3d208ae
Hugo <abgonon@lemonde.fr>
2014-12-22 14:20:11 +0100

    save display preference in localstorage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,7 +6,10 @@ angular.module('gtrApp')
     $scope.teams = config.teams;
     $scope.team  = team;
     $scope.wallDisplay = config.wallDisplay;
-    $scope.display = 'list';
+    $scope.display = localStorage.getItem('display') || 'list';
+    $scope.$watch('display', function(displayValue) {
+      localStorage.setItem('display', displayValue);
+    });
 
     if (typeof(config.teams[team].descendingOrder) !== 'undefined') {
       $scope.descendingOrder = config.teams[team].descendingOrder;
